4e274510ad3d24ff354587cf3ffc6125c16ec537
PSob888 <235975@edu.p.lodz.pl>
2023-05-03 00:36:47 +0200

    Added ViewPager and stuff

--- app/src/main/java/com/example/weatherapp2/MainActivity.java
+++ app/src/main/java/com/example/weatherapp2/MainActivity.java
@@ -3,14 +3,22 @@ package com.example.weatherapp2;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.annotation.SuppressLint;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager2 viewPager = findViewById(R.id.viewpager);
+        PageAdapter pageAdapter = new PageAdapter(this);
+        viewPager.setAdapter(pageAdapter);
     }
 }
--- app/src/main/java/com/example/weatherapp2/PageAdapter.java
+++ app/src/main/java/com/example/weatherapp2/PageAdapter.java
@@ -1,7 +1,12 @@
 package com.example.weatherapp2;
 
+import android.annotation.SuppressLint;
 import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -11,22 +16,42 @@ import androidx.lifecycle.Lifecycle;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 public class PageAdapter extends FragmentStateAdapter {
-    public PageAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
-        super(fragmentManager, lifecycle);
+
+    private FragmentActivity mainActivity;
+    public PageAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+        this.mainActivity = fragmentActivity;
     }
 
+    @SuppressLint("ResourceAsColor")
     @NonNull
     @Override
     public Fragment createFragment(int position) {
+        EditText editText = mainActivity.findViewById(R.id.editTextTextPersonName);
+        ImageButton imageSearch = mainActivity.findViewById(R.id.imageButtonSearch);
+        ImageButton imageFav = mainActivity.findViewById(R.id.imageButtonFav);
+        ImageButton imageMenu = mainActivity.findViewById(R.id.imageButtonMenu);
         switch(position){
             case 0:
+                editText.setBackgroundColor(0x20FFFFFF);
+                imageSearch.setBackgroundColor(0x20FFFFFF);
+                imageFav.setBackgroundColor(0x00FFFFFF);
+                imageMenu.setBackgroundColor(0x00FFFFFF);
                 return new FragmentWeather();
             case 1:
+                editText.setBackgroundColor(0x00FFFFFF);
+                imageSearch.setBackgroundColor(0x00FFFFFF);
+                imageFav.setBackgroundColor(0x20FFFFFF);
+                imageMenu.setBackgroundColor(0x00FFFFFF);
                 return new FragmentFavourite();
             case 2:
+                editText.setBackgroundColor(0x00FFFFFF);
+                imageSearch.setBackgroundColor(0x00FFFFFF);
+                imageFav.setBackgroundColor(0x00FFFFFF);
+                imageMenu.setBackgroundColor(0x20FFFFFF);
                 return new FragmentSettings();
             default:
-                return null;
+                throw new Resources.NotFoundException("Position not found");
         }
     }
 
