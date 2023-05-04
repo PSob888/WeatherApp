7eb9ebcaf041ceeec808bdfa8c66a25f9bda58c4
PSob888 <235975@edu.p.lodz.pl>
2023-05-04 15:02:40 +0200

    Dodany spinner ale cos sie wywala, pewnie problem z viewpagerem

--- app/src/main/java/com/example/weatherapp2/FragmentSettings.java
+++ app/src/main/java/com/example/weatherapp2/FragmentSettings.java
@@ -1,5 +1,6 @@
 package com.example.weatherapp2;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -7,11 +8,16 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class FragmentSettings extends Fragment {
 
@@ -32,6 +38,10 @@ public class FragmentSettings extends Fragment {
 
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        Spinner spinnerTime =view.findViewById(R.id.spinnerTime);
+
+        initSpinner(spinnerTime);
     }
 
     @Override
@@ -45,5 +55,14 @@ public class FragmentSettings extends Fragment {
         imageFav.setBackgroundColor(0x00FFFFFF);
         imageMenu.setBackgroundColor(0x20FFFFFF);
         super.onResume();
+    }
+
+    private void initSpinner(Spinner spinnerTime) {
+        String[] items = new String[]{
+                "5 minutes", "10 minutes", "30 minutes", "1 hour",
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, items);
+        spinnerTime.setAdapter(adapter);
     }
 }
